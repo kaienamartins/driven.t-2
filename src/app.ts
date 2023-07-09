8652877a10cbba70befcb65302d9b710e447fa03
Kaiena <kaienafmartins@gmail.com>
2023-07-09 14:39:39 -0300

    fix: app

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,14 @@ import { loadEnv, connectDb, disconnectDB } from '@/config';
 loadEnv();
 
 import { handleApplicationErrors } from '@/middlewares';
-import { usersRouter, authenticationRouter, eventsRouter, enrollmentsRouter } from '@/routers';
+import {
+  usersRouter,
+  authenticationRouter,
+  eventsRouter,
+  enrollmentsRouter,
+  ticketsRouter,
+  paymentRouter,
+} from '@/routers';
 
 const app = express();
 app
@@ -19,7 +26,9 @@ app
   .use('/auth', authenticationRouter)
   .use('/event', eventsRouter)
   .use('/enrollments', enrollmentsRouter)
-  .use(handleApplicationErrors);
+  .use(handleApplicationErrors)
+  .use('/tickets', ticketsRouter)
+  .use('/payments', paymentRouter);
 
 export function init(): Promise<Express> {
   connectDb();
